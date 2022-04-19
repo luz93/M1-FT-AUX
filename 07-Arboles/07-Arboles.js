da7e5e014f8accfacd36a84b4e9c12fc3eac3a5b
Luz Carrizo <rsrqr>
2022-04-19 20:43:52 -0300

    termine test:07

--- 07-Arboles/07-Arboles.js
+++ 07-Arboles/07-Arboles.js
@@ -8,8 +8,18 @@ const { BinarySearchTree } = require("../estructuras")
 //         /  \      /  \
 //      (2)  (9)  (11)  (15)   
 BinarySearchTree.prototype.search= function(value){
-   
-}
+
+        if(this.value === value) return this.value; //caso base
+        while(this.right || this.left){ //iteracion
+                if(value > this.value){
+                return this.right.search(value) //recursion
+                    }else{
+                return this.left.search(value) //recursion
+                }
+            }
+        return "no se encontró el valor"
+        }
+
 
 
 // En el prototipo de BinarySearchTree crear la funcion height que debe retornar la altura del mismo (cantidad de niveles)
@@ -20,9 +30,27 @@ BinarySearchTree.prototype.search= function(value){
 //      (2)   (9)  (11)  (15)   ----> nivel 2
 
 
-BinarySearchTree.prototype.height= function(){
- 
-}
+BinarySearchTree.prototype.height= function(height = 1){
+        // if(!this.right && !this.left) {
+        //     // No hay elementos, devolver profudidad actual
+        //     return height;
+        // } else {
+        //     // Calcular profundidad máxima en cada extremo con asignación ternaria
+        //     // Si el extremo no tiene elementos, se mantiene profundidad actual
+        //     let lHeight = (!this.left) ? height : this.left.height(height + 1);
+        //     let rHeight = (!this.right) ? height : this.right.height(height + 1);
+        //     // Se devuelve el mayor de ambos extremos
+        //     height = Math.max(lHeight, rHeight);
+        // }
+        // return height;
+        if(!this.left&&!this.right) return 0;
+ else if(this.right && !this.left) return 0 + this.right.size()
+ else if(!this.right && this.left) return 0 + this.left.size()
+ else if(this.right.size()>this.left.size()) return this.right.height() + 1
+ else if (this.right.size()<this.left.size())return this.left.height() + 1
+  else if (this.right.size() === this.left.size()) return this.right.height() + 1
+
+    }
 
 module.exports={
     BinarySearchTree
