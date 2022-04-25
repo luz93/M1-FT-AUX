9aa210d18f0137461aae4c1cd175304b4d4889bc
Luz Carrizo <rsrqr>
2022-04-25 15:26:19 -0300

    homework test 08

--- 08-Algoritmos/08-Algoritmos.js
+++ 08-Algoritmos/08-Algoritmos.js
@@ -34,7 +34,26 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  
+  if(array.length <= 1){ // [ 5, 1, 4, 2, 8]
+    return array;
+  }
+  // ubicamos de forma aleatoria el pivot:
+  var pivot = Math.floor((Math.random()*array.length)); // pivot = 1
+  // La función Math.random() retorna un punto flotante, 
+  // un número pseudo-aleatorio dentro del rango [0, 1).
+  // Math.floor() devuelve el máximo entero menor o igual a un número.
+  var right = []; // array = [left , pivot, right]  [5, 4, 2, 8 ]
+  var left = []; // []
+  for(let i = 0; i < array.length ; i++){ // recorremos el array
+    if(i !== pivot) {
+      if(array[i] < array[pivot]) {
+        left.push(array[i]);        
+      } else {
+        right.push(array[i]);
+      }
+    }
+  }  
+  return [].concat(quickSort(left), array[pivot], quickSort(right)); // recursion
 
 
 }
